7b1b24ed98331d12102afa995d950453e663fae4
id16jhd <id16jhd@cs.umu.se>
2019-05-14 13:51:25 +0200

    Updates

---
+++ precache-manifest.a73b0da653e9fd45d8ba634cca7ab2b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "18d918f83252776fb96f161b61559bc1",
+    "url": "/portfolio/static/media/square.18d918f8.jpg"
+  },
+  {
+    "revision": "46bd20089e8c8b10123dec7b9153695a",
+    "url": "/portfolio/static/media/profilbild.46bd2008.jpg"
+  },
+  {
+    "revision": "67d30f6fe9abfd2a1acf7bbe3203633d",
+    "url": "/portfolio/static/media/logo.67d30f6f.png"
+  },
+  {
+    "revision": "54965a68bb3d54e69e168bca68fc1deb",
+    "url": "/portfolio/static/media/IMG_0599.54965a68.JPG"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c1ce02ebc158d4943a87",
+    "url": "/portfolio/static/js/main.b0ccf02d.chunk.js"
+  },
+  {
+    "revision": "f5a877060688e3c4042a",
+    "url": "/portfolio/static/js/2.37c41ef4.chunk.js"
+  },
+  {
+    "revision": "c1ce02ebc158d4943a87",
+    "url": "/portfolio/static/css/main.65d6fa2e.chunk.css"
+  },
+  {
+    "revision": "80db70c23849d84502845cd7fcc1cedb",
+    "url": "/portfolio/index.html"
+  }
+];
